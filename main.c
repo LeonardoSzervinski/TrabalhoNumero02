d15ed24dd8e91189b633f76f3c788118256af80e
szervinski <leonardo.szervinski@hotmail.com>
2023-09-17 16:26:10 -0300

    main.c
    add função `partition` a função implementa a etapa de partição do algoritmo Quicksort.

--- main.c
+++ main.c
@@ -6,3 +6,20 @@ void swap(char **a, char **b) {
     *a = *b;
     *b = temp;
 }
+
+int partition(char **arr, int low, int high, int *trocas, int *comparacoes) {
+    char *pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+        (*comparacoes)++;
+        if (strcmp(arr[j], pivot) < 0) {
+            i++;
+            (*trocas)++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    (*trocas)++;
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
